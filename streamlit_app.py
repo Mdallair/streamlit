4fdb13acc3b11e2bcf9ca2e790a64612cbb39f7c
Mdallair <145697258+Mdallair@users.noreply.github.com>
2025-03-17 02:21:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ st.write("### (1) add a drop down for Category (https://docs.streamlit.io/librar
 categories = df['Category'].unique().tolist()
 selected_category = st.selectbox("Select a Category", categories)
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)*")
-df1 = df[df['category'] == selected_category]
-subcategories = df1['subcategory'].unique().tolist()
+df1 = df[df['Category'] == selected_category]
+subcategories = df1['Sub_Category'].unique().tolist()
 selected_subcategories = st.multiselect("Select Subcategories", subcategories)
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
